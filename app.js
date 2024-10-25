2752728bbbccab83e192f65415e58bd0d48733bd
Fernando Melo <fernando.omelo@hotmail.com>
2024-10-25 18:40:52 -0300

    Implementando Lógica do sistema.

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
-function sortear(){
-    let edtQuantidade = parseInt(document.getElementById('quantidade').value);
-    let edtMinimo = parseInt(document.getElementById('de').value);
-    let edtMaximo = parseInt(document.getElementById('ate').value);
-    
-    let numerosSorteados = [];
+let edtQuantidade = document.getElementById('quantidade'); 
+let edtMinimo = document.getElementById('de'); 
+let edtMaximo = document.getElementById('ate');
+let btnReiniciar = document.getElementById('btn-reiniciar');
+let resultado = document.getElementById('resultado');
+let numero;
+let numerosSorteados = [];
 
-    for(let i = 0; i < edtQuantidade; i++){
-        numerosSorteados.push(obterNumeroAleatorio(edtMinimo, edtMaximo));
+function sortear(){
+    let AQuantidade = parseInt(edtQuantidade.value);
+    let AMinimo = parseInt(edtMinimo.value);
+    let AMaximo = parseInt(edtMaximo.value);
+
+    for(let i = 0; i < AQuantidade; i++){
+         numero = obterNumeroAleatorio(AMinimo, AMaximo);
+        while(numerosSorteados.includes(numero)){
+            numero = obterNumeroAleatorio(AMinimo, AMaximo);
+        }
+        numerosSorteados.push(numero); 
     }
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${numerosSorteados}</label>`
+    btnReiniciar.className = 'container__botao';
 }
 
 function reiniciar(){
-
+    numero = 0;
+    numerosSorteados = [];
+    edtQuantidade.value = '';
+    edtMinimo.value = '';
+    edtMaximo.value = '';
+    resultado.innerHTML = ' <label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    btnReiniciar.className = 'container__botao-desabilitado';
 }
 
 function obterNumeroAleatorio(AMinimo, AMaximo){
